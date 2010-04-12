cbcea203be133d8a58d71140ba691eab0e8804fa
cdotyone <cdoty1@yahoo.com>
2010-04-12 07:36:29 -0500

    added reference to new plug-in

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,13 +1,9 @@
 import org.apache.catalina.loader.WebappLoader
 
 eventConfigureTomcat = {tomcat ->
-  println "### Starting load of custom application"
   def contextRoot = "/mochaui"
   def buildroot= "/mochaui-grails/WEB-INF/classes"
   def webroot  = new File('../mochaui/demo').getCanonicalPath()
-
-  println buildroot
-  println webroot
 
   context = tomcat.addWebapp(contextRoot, webroot);
   context.reloadable = true
@@ -17,7 +13,5 @@ eventConfigureTomcat = {tomcat ->
   loader.addRepository(new File(buildroot).toURI().toURL().toString());
   context.loader = loader
   loader.container = context
-
-  println "### Ending load of custom application"
 }
 
