6a843cce4d57415ad0dd072d247a42e4c3795d0b
cdotyone <cdoty1@yahoo.com>
2010-04-14 02:29:39 -0500

    file change event almost complete

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,4 +1,7 @@
 import org.apache.catalina.loader.WebappLoader
+import net.contentobjects.jnotify.*
+
+def watchID = -1
 
 eventConfigureTomcat = {tomcat ->
   def contextRoot = "/mochaui"
@@ -13,5 +16,24 @@ eventConfigureTomcat = {tomcat ->
   loader.addRepository(new File(buildroot).toURI().toURL().toString());
   context.loader = loader
   loader.container = context
+
+
+  // initialize file change notifications
+  def path = new File("d:\\Data\\java\\mochaui\\src\\").getCanonicalPath()
+  def mask = JNotify.FILE_CREATED  |
+              JNotify.FILE_DELETED  |
+              JNotify.FILE_MODIFIED |
+              JNotify.FILE_RENAMED;
+
+  watchID = JNotify.addWatch(path, mask, true, new FileChangeWatcher())  
+}
+
+eventExiting = {
+  if(watchID>-1) JNotify.removeWatch(watchID)
+}
+
+eventBuildMocha = { path->
+  println 'eventBuildMocha'
+  println path
 }
 
---
+++ src/groovy/FileChangeListener.groovy
@@ -0,0 +1,41 @@
+import net.contentobjects.jnotify.JNotifyListener
+import java.util.concurrent.Semaphore
+
+includeTargets << grailsScript("_GrailsInit")
+includeTargets << grailsScript("_GrailsEvents")
+
+class FileChangeWatcher implements JNotifyListener {
+    def onlyone = new Semaphore(1)
+
+    // fires events, and suppresses double messages from OS
+    def doChangeNotify = { change,rootPath,oldName,newName ->
+      def prevChange
+
+      if(onlyone.tryAcquire()) {
+        Thread.start {
+          Thread.sleep(2000)
+
+          if(change=="renamed") println "renamed " + rootPath + " : " + oldName + " -> " + newName
+          if(change=="modified") println "modified " + rootPath + " : " + oldName
+          if(change=="deleted") println "deleted " + rootPath + " : " + oldName
+          if(change=="created")  println "created " + rootPath + " : " + oldName
+          event("BuildMocha", [rootPath])
+
+          onlyone.release()
+        }
+      }
+    }
+
+    def void fileRenamed(int wd, String rootPath, String oldName, String newName) {
+      doChangeNotify("renamed",rootPath,oldName,newName)
+    }
+    def void fileModified(int wd, String rootPath, String name) {
+      doChangeNotify("modified",rootPath,name,name)
+    }
+    def void fileDeleted(int wd, String rootPath, String name) {
+      doChangeNotify("deleted",rootPath,name,name)
+    }
+    def void fileCreated(int wd, String rootPath, String name) {
+      doChangeNotify("created",rootPath,name,name)
+    }
+}
